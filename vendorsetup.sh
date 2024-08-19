0a1a39d1ef09774a7c23937e7f3d795c5b31415b
Alucard Stormbringer <supersayan1000@gmail.com>
2024-08-19 07:01:29 +0530

    haydn: add patches
    and switch to new derpfest-14 branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Common Tree
-git clone --depth=1 https://github.com/Alucard-Storm/android_device_xiaomi_sm8350-common -b derp device/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/Alucard-Storm/android_device_xiaomi_sm8350-common -b derpfest-14 device/xiaomi/sm8350-common
 
 # Haydn Vendor
 git clone --depth=1 https://github.com/Alucard-Storm/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
@@ -25,5 +25,33 @@ rm -rf hardware/xiaomi && git clone --depth=1 https://github.com/LineageOS/andro
 echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
+
+# Leica patch
+echo 'Adding Leica camera patch'
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+patch -p1 <0001-Add-backwards-compatible-CaptureResultExtras-constructor.patch
+cd ../..
+
+# Audio
+echo 'Adding audio-app support'
+cd system/core
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Audio-app-main/0001-Add-audio-app-support.patch
+patch -p1 <0001-Add-audio-app-support.patch
+cd ../..
+
+# Dolby
+echo 'Adding Dolby patch'
+cd frameworks/av
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Dolby/0004-Import-Dolby-Effects-initialization.patch
+patch -p1 <0004-Import-Dolby-Effects-initialization.patch
+cd ../..
+
+# Optimization
+echo 'Adding optimization patch'
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Optimization/0001-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
+patch -p1 <0001-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
+cd ../..
 
 echo 'delete vendorsetup.sh from device tree once this is done'
